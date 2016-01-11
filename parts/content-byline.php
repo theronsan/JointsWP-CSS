413ea87175d8041ea288ed347af22509a5a220c6
theronsan <theronsan@gmail.com>
2016-01-11 20:46:10 +0100

    Line 5
    I got an error from this file and I think that line 5 should be:
    joints_get_the_author_posts_link()

--- parts/content-byline.php
+++ parts/content-byline.php
@@ -2,7 +2,7 @@
 	/* Translators: %1$s is the time, %2$s is the link to the author's archive, %3$s is the category */
 	printf( '<p class="byline">' . __( 'Posted on %1$s by %2$s - %3$s', 'jointswp' ) . '</p>',
 		get_the_time( 'F j, Y' ),
-		get_the_author_posts_link(),
+		joints_get_the_author_posts_link(),
 		get_the_category_list( __( ', ', 'jointswp' ) )
 	);
 ?>
